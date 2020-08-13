d1176f9df26e164150839fc741de0b64e1c3c5fa
mahmeds7777 <mahmeds7777@gmail.com>
2020-08-13 07:01:51 -0400

    Updates

---
+++ precache-manifest.6de0d311821a9e47edc251747f667723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffec035a60928d433bd45cb0ac19c6cf",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "e236bb161e9ae9908994",
+    "url": "/simple-todo-app/static/css/main.202285f5.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e236bb161e9ae9908994",
+    "url": "/simple-todo-app/static/js/main.69443c97.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
